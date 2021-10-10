24ba54be21ff842c90fbded6469152483949a5e6
Lilian Martins Fritzen <liliancmartinsfritzen@gmail.com>
2021-10-09 23:37:30 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div>
+      <h1>Inspire-se com a frase de hoje!</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/citation/Phrases.jsx
@@ -0,0 +1,18 @@
+export const Phrases = {
+
+}
+
+// 'A gente só encanta quando se encanta. Se eu não estiver encantado com o 
+//  meu objeto de conhecimento, eu não posso encantar o outro.'
+
+// 'O impossível não é um fato, é uma OPINIÃO.'
+
+// 'Gente que não tem dúvida, só é capaz de repetir.'
+
+// 'Na vida, nós devemos ter raízes, e não âncoras. Raiz alimenta, âncora imobiliza.'
+
+// 'Humilde é aquela pessoa que sabe que não sabe tudo, que sabe que outra pessoa
+// sabe o que ela não sabe, que ela e outra pessoa saberão muitas coisas juntas,
+// que ela e outra pessoa nunca saberão tudo que pode ser SABIDO.'
+
+// 'Coragem é preparo e não mera disposição eufórica.'
